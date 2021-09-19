d800d0d62208f93989eea2789d6ba604fd847cd0
computerprogrammingmonkey <90799938+computerprogrammingmonkey@users.noreply.github.com>
2021-09-19 11:50:19 -0500

    Create playerinteraction.js

---
+++ playerinteraction.js
@@ -0,0 +1,24 @@
+var angle = 400;
+var scalar = 150;
+var startX = 200;
+var startY = 200;
+var Decider = false;
+
+function setup(){
+    angleMode(degrees);
+}
+
+draw = function() {
+    var x = startX + scalar * cos(angle);
+    var y = startY + scalar * sin(angle);
+    background(255, 255, 255);
+    ellipse(200, 200, 300, 300);
+    ellipse(x, y,25,25  );
+    if(keyIsPressed && keyCode === LEFT){
+        
+    angle++;
+    }
+    else if(keyIsPressed && keyCode === RIGHT){
+        angle--;
+    }
+};
